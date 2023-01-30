c8f08c37b33400d7cf04004af7efc2291d01ce9c
Jomari Abejo <jomariabejo@gmail.com>
2023-01-30 21:19:20 +0800

    second commit

--- src/com/initial/Main.java
+++ src/com/initial/Main.java
@@ -1,17 +1,9 @@
+package com.initial;
+
 import java.sql.*;
 public class Main {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/books";
-        String username = "root";
-        String password = "";
-
-        Connection con = DriverManager.getConnection(url, username, password);
-        if (con != null) {
-            System.out.println("Connection established");
-        } else {
-            System.out.println("Failed to make connection");
-        }
-
-
+        SelectAllExample sample = new SelectAllExample();
+        sample.printBooksDetails();
     }
 }
