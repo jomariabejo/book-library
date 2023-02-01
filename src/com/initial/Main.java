df6b70eb64acbb21631e242701506b3cf48a5a5e
Jomari Abejo <jomariabejo@gmail.com>
2023-02-01 08:40:37 +0800

    update

--- src/com/initial/Main.java
+++ src/com/initial/Main.java
@@ -1,9 +1,12 @@
 package com.initial;
 
 import java.sql.*;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) throws SQLException {
-        SelectAllExample sample = new SelectAllExample();
+        CRUD sample = new CRUD();
         sample.printBooksDetails();
+        sample.INSERT("Clean Code: A Handbook of Agile Software Craftsmanship","Robert Cecil","Martin","2008","100","464");
     }
 }
