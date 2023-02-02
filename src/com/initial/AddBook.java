af7ce0e0ceab1f220f83093f4b8df46ec789e2a6
Jomari Abejo <jomariabejo@gmail.com>
2023-02-02 14:47:46 +0800

    AddBook.java was created as a new file with attributes, setters, and getters.

---
+++ src/com/initial/AddBook.java
@@ -0,0 +1,58 @@
+package com.initial;
+
+public class AddBook {
+    String title,
+            author_fname,
+            author_lname,
+            released_year,
+            stock_quantity,
+            pages;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor_fname() {
+        return author_fname;
+    }
+
+    public void setAuthor_fname(String author_fname) {
+        this.author_fname = author_fname;
+    }
+
+    public String getAuthor_lname() {
+        return author_lname;
+    }
+
+    public void setAuthor_lname(String author_lname) {
+        this.author_lname = author_lname;
+    }
+
+    public String getReleased_year() {
+        return released_year;
+    }
+
+    public void setReleased_year(String released_year) {
+        this.released_year = released_year;
+    }
+
+    public String getStock_quantity() {
+        return stock_quantity;
+    }
+
+    public void setStock_quantity(String stock_quantity) {
+        this.stock_quantity = stock_quantity;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+}
