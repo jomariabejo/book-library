139f2757648ef7c4093f9f1d4da4f6c688386241
Jomari Abejo <jomariabejo@gmail.com>
2023-02-02 18:46:21 +0800

    created a readbook to locate data within a database table

---
+++ src/com/initial/ReadBook.java
@@ -0,0 +1,14 @@
+package com.initial;
+import java.sql.*;
+
+public class ReadBook {
+    public void FindBookByTitle(String title) {
+
+    }
+    public void FindBookByID(String ID) {
+
+    }
+    public void FindBookByAuthorFullName(String fullname) {
+
+    }
+}
