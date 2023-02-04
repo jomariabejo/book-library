090b528f525dc0ad5f48c2ee229fcdf52c006316
Jomari Abejo <jomariabejo@gmail.com>
2023-02-05 07:07:48 +0800

    added user(getter and setterreset), UserAttributes and InsertUser

--- src/com/initial/Create.java
+++ src/com/initial/Create.java
@@ -11,12 +11,56 @@ public class Create {
 
     static final String USER = "root";
     static final String PASS = "";
+    // BOOK
     String title,
             author_fname,
             author_lname,
             released_year,
             stock_quantity,
             pages;
+
+    public String getUserUsername() {
+        return userUsername;
+    }
+
+    public void setUserUsername(String userUsername) {
+        this.userUsername = userUsername;
+    }
+
+    public String getUserFirstName() {
+        return userFirstName;
+    }
+
+    public void setUserFirstName(String userFirstName) {
+        this.userFirstName = userFirstName;
+    }
+
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    // USER
+    private String userUsername,userFirstName,userLastName,userEmail,userPassword;
     public String getTitle() {
         return title;
     }
@@ -93,9 +137,48 @@ public class Create {
                 e.printStackTrace();
             }
         }
-        System.out.println("Thank you for INSERTING new data, Goodbye!");
+        System.out.println("Thank you for INSERTING new BOOK data, Goodbye!");
+    }
+    public void InsertUser(){
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+            String fnal_start = "INSERT INTO `users`(`username`, `firstname`, `lastname`, `email`, `password`) ";
+            String fnal_end = " VALUES (" + "'" + getUserUsername() + "'" + ",'" + getUserFirstName() + "'" + ',' + "'" + getUserLastName() + "'" + ",'" + getUserEmail() + "'" + ",'" + getUserPassword() + "'" + ")";
+            String sql = fnal_start + fnal_end;
+
+            int rowsAffected = stmt.executeUpdate(sql);
+            System.out.println(rowsAffected + " row(s) affected.");
+        }
+        catch (ClassNotFoundException | SQLException e) {
+
+        }
+        finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Thank you for INSERTING new USER data, Goodbye!");
     }
     public void resetBooksAttributes(){
+        setTitle(null);
+        setPages(null);
+        setAuthor_lname(null);
+        setAuthor_fname(null);
+        setPages(null);
+        System.out.println("Attributes set to NULL");
+    }
+    public void resetUsersAttributes(){
         setTitle(null);
         setPages(null);
         setAuthor_lname(null);
