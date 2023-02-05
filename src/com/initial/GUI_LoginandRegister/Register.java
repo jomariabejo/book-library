cbb49d61ca959408eb09752e3b6783a698b72bbb
Jomari Abejo <jomariabejo@gmail.com>
2023-02-05 21:14:52 +0800

    To run the query, I added setters and called InsertUser().

--- src/com/initial/GUI_LoginandRegister/Register.java
+++ src/com/initial/GUI_LoginandRegister/Register.java
@@ -1,10 +1,12 @@
 package com.initial.GUI_LoginandRegister;
 
+import com.initial.Create;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Register extends JFrame{
+public class Register extends JFrame {
     private JTextField fName_txtField;
     private JTextField lName_txtField;
     private JTextField eMail_txtField;
@@ -13,7 +15,7 @@ public class Register extends JFrame{
     private JPanel panelMain;
     private JPasswordField passwordField_password;
     private JPasswordField passwordField_Confirm;
-    private JProgressBar progressBar1;
+    private JTextField username_txtField;
 
     public Register() {
 
@@ -22,7 +24,7 @@ public class Register extends JFrame{
         setIconImage(img.getImage());
         setContentPane(panelMain);
         setTitle("Jomari Abejo");
-        setSize(400,600);
+        setSize(400, 600);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(false);
@@ -30,7 +32,33 @@ public class Register extends JFrame{
         registerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Register");
+                Create create = new Create();
+                System.out.println(create.theCheckIfExist());
+
+                if (String.valueOf(passwordField_password.getPassword()).equals(String.valueOf(passwordField_Confirm.getPassword()))) {
+                    create.setUserUsername(username_txtField.getText());
+                    create.setUserFirstName(fName_txtField.getText());
+                    create.setUserLastName(lName_txtField.getName());
+                    create.setUserEmail(eMail_txtField.getText());
+                    create.setUserPassword(String.valueOf(passwordField_password.getPassword()));
+                    create.InsertUser();
+
+                    /*
+                        Account Already Exist
+                     */
+                    if (create.getCheckIfExist() != null) {
+                        JOptionPane.showMessageDialog(panelMain, "Account Exist");
+                    }
+                    /*
+                        Account Successfuly Registered to Database
+                     */
+                    if (create.getCheckIfExist() == null) {
+                        JOptionPane.showMessageDialog(panelMain, "Successfully Registered");
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(panelMain, "Password didn't match");
+                }
+
             }
         });
         backToLoginButton.addActionListener(new ActionListener() {
