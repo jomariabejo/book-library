54f762185e1d2472bfa3abd05d49b1c9965cc218
Jomari Abejo <jomariabejo@gmail.com>
2023-02-17 11:15:12 +0800

    Initial code login gui

--- Main/src/com/main/main.java
+++ Main/src/com/main/main.java
@@ -5,6 +5,13 @@ import  com.create.CREATE;
 import  com.delete.DELETE;
 import  com.read.READ;
 import  com.update.UPDATE;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.lang.reflect.Type;
 import  java.sql.SQLException;
 
 public class main {
@@ -29,5 +36,103 @@ public class main {
         Users users = new Users();
 
         admin.greeting("Jomari Abejo");
+
+
+        //  LOGIN GUI
+        LOGIN login = new LOGIN();
+        REGISTER register = new REGISTER();
+        FORGOTPASSWORD forgotpassword = new FORGOTPASSWORD();
+
+
+        //  USERS GUI
+        USERS_HOMEPAGE users_homepage = new USERS_HOMEPAGE();
+
+
+        //  ADMIN GUI
+        ADMIN_HOMEPAGE admin_homepage = new ADMIN_HOMEPAGE();
+
+
+        /*
+         * The program will start to login page.
+         */
+
+    /*
+        LOGIN GUI START
+     */
+
+        login.setVisible(true);
+        login.getBtn_login().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Login Clicked");
+
+                String username = login.getTxtField_username().getText();
+                String password = String.valueOf(login.getPwdField_password().getPassword());
+
+                read.check_admin_or_user(username,password);
+
+                if (read.isAdmin()){
+                    JOptionPane.showMessageDialog(login.getPanelMain(),"Welcome back admin");
+                    admin_homepage.setVisible(true);
+                    login.dispose();
+                }
+                else if (read.isUser()){
+                    JOptionPane.showMessageDialog(login.getPanelMain(),"Welcome back " + username);
+                    users_homepage.setVisible(true);
+                    login.dispose();
+                }
+                else if (read.isPassword()){
+                    JOptionPane.showMessageDialog(login.getPanelMain(),"Account not found");
+                    login.getTxtField_username().setText("");
+                    login.getPwdField_password().setText("");
+                }
+            }
+        });
+        login.getBtn_register().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Register Clicked");
+                register.setVisible(true);
+                login.dispose();
+            }
+        });
+        login.getBtn_login().addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                if (e.getKeyCode() == 0){
+                    System.out.println("Enter Pressed");
+                }
+            }
+        });
+        login.getBtn_register().addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                if (e.getKeyCode() == 0){
+                    System.out.println("Enter Pressed");
+                }
+            }
+        });
+
+    /*
+        LOGIN GUI END
+     */
+
+
+
+
+    /*
+        REGISTER GUI END
+     */
+
+
+    /*
+        REGISTER GUI END
+     */
+
+
+
+
     }
 }
